635ee5e26c54a3656f66350b3ead663c88ad4b5a
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-06-27 23:03:15 +0530

    Update 05_strings.js

--- src/01_Basics/05_strings.js
+++ src/01_Basics/05_strings.js
@@ -1,22 +1,24 @@
 const name = "sarfaraz";
 const repoCount = 50;
-// console.log(name + repoCount + " Value");
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String("Sarfaraz");
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__); //visible in browser
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2));
-// console.log(gameName.indexOf('z'));
+//methods in prototype
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('z'));
 
 const newString = gameName.substring(0, 4);
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4);
+const anotherString = gameName.slice(-8, 4); 
 console.log(anotherString);
 
 const newStringOne = "  Sarfaraz.     ";
@@ -30,3 +32,28 @@ console.log(url.includes("hitesh"));
 
 const newStringTwo = "sarfaraz-hussain-chaudhary";
 console.log(newStringTwo.split("-"));
+
+//Output
+// sarfaraz50 Value
+// Hello my name is sarfaraz and my repo count is 50
+
+// S
+// {}
+
+// 8
+// SARFARAZ
+// r
+// 7
+
+// Sarf
+
+// Sarf
+
+//   Sarfaraz.     
+// Sarfaraz.
+
+// https://hitesh.com/hitesh-chodhary
+
+// true
+
+// [ 'sarfaraz', 'hussain', 'chaudhary' ]
