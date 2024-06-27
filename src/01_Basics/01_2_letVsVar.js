51a75f17ccff01c8945fb4a8c3de03f465c31b41
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-06-27 16:43:58 +0530

    Create 01_2_letVsVar.js

---
+++ src/01_Basics/01_2_letVsVar.js
@@ -0,0 +1,38 @@
+var a = 5; //function-scoped: available throughout
+let b = 5; //block-scoped only
+
+function varScoping() {
+    var x = 1;
+  
+    if (true) {
+      var x = 2;
+      console.log(x); // will print 2
+    }
+  
+    console.log(x); // will print 2
+  }
+  
+  function letScoping() {
+    let x = 1;
+  
+    if (true) {
+      let x = 2;
+      console.log(x); // will print 2
+    }
+  
+    console.log(x); // will print 1
+  }
+
+varScoping();
+letScoping();
+
+function varAndLetScoping() {
+    var x = 1;
+  
+    if (true) {
+      let x = 2;
+      console.log(x); // will print 2
+    }
+  
+    console.log(x); // will print 1
+  }
