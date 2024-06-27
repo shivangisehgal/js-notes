7d39fa76a31c0fec20e1431399bf86f902739d89
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-06-27 22:35:15 +0530

    Update datatypes-summary.js

--- src/01_Basics/datatypes-summary.js
+++ src/01_Basics/datatypes-summary.js
@@ -1,7 +1,6 @@
-//primitive
+/////////////////////// primitive //////////////////////
 const score = 100;
 const scoreValue = 100.3;
-
 const isLoggedIn = false;
 const outsideTemp = null;
 let userEmail;
@@ -9,39 +8,23 @@ let userEmail;
 const id = Symbol("123");
 const anotherId = Symbol("123");
 
-console.log(id === anotherId);
+console.log(id === anotherId); //false
 
 const bigNumber = 2323232323232323232323232n;
 
-//Reference (Non-primitive datatype)
+/////////////////////// refernece //////////////////////
+
+//Array
 const heros = ["shaktiman", "naagraj", "doga"];
 
+//Object
 let myObj = {
   name: "sarfaraz",
   age: 17
 };
 
+//function
 const myFunction = function () {
   console.log("Hello world");
 };
 
-// console.log(typeof bigNumber);
-// console.log(typeof heros);
-// console.log(typeof myFunction);
-
-let myYoutubeName = "hiteshchoudharydotcom";
-let anothername = myYoutubeName;
-anothername = "chaiaurcode";
-console.log(myYoutubeName);
-console.log(anothername);
-
-let userOne = {
-  email: "user@gmail.com",
-  upi: "user@ybl"
-};
-
-let userTwo = userOne;
-
-userTwo.email = "hitesh";
-console.log(userOne.email);
-console.log(userTwo.email);
