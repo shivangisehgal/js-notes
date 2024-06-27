494bcb26bc27bd20170443f58a06983ffb9e8866
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-06-27 17:27:28 +0530

    Update 04_comparison.js

--- src/01_Basics/04_comparison.js
+++ src/01_Basics/04_comparison.js
@@ -1,18 +1,55 @@
-// console.log(2 > 1);
-// console.log(2 >=1);
-// console.log(2 <1);
-// console.log(2==1);
-// console.log(2!=1);
-
 console.log("2" > 1);
 console.log("02" > 1);
+console.log("1" > 1);
+console.log("1" == 1);
+console.log("1" === 1);
+
+console.log("\n");
+
+
+console.log(true > 0);
+console.log(true < 0);
+console.log(false == 0);
+console.log(false > 0);
+console.log(true == 1);
+console.log(true > 1);
+
+console.log("\n");
+
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
+console.log("\n");
+
+
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-//---------------- ==== Strict check ----------
+//Output
+
+// true
+// true
+// false
+// true
+// false
+
+
+// true
+// false
+// true
+// false
+// true
+// true
+
+
+// false
+// false
+// true
+
+
+// false
+// false
+// false
