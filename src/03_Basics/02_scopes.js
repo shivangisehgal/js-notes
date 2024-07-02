632e5182dfb8c2c14700143149cd5c22cc234917
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-07-02 22:24:38 +0530

    Update 02_scopes.js

--- src/03_Basics/02_scopes.js
+++ src/03_Basics/02_scopes.js
@@ -1,64 +1,77 @@
-// let a = 10;
-// const b = 20;
-// var c = 30;
+let a = 10;
+const b = 20;
+var c = 30;
 
-// var c = 300;
-let a = 300;
+var c = 300;
+//let a = 300; //error
 
 if (true) {
-  let a = 10;
-  const b = 20;
-  // var c = 30;
+  let a = 11;
+  const b = 30;
+  var c = 40;
   // console.log("Inner: ", a);
 }
 
-// console.log(a)
-// console.log(b)
-// console.log(c)
+console.log(a)
+console.log(b)
+console.log(c)
+
 
 //-------------- Scope level and mini hoisting ----------------
 
-//Scope using functions
+// //Scope using functions
+
 function one() {
   const username = "hitesh";
 
   function two() {
     const website = "youtube";
-    // console.log("Inner ",username);
+    console.log(username); //hitesh
   }
-  // console.log(website);
+  
+  //console.log(website); //error
   two();
 }
 
 one();
 
+//--------------
+
 // Scope using if statements
+
 if (true) {
   const username = "hitesh";
+  
   if (username === "hitesh") {
     const website = "   youtube";
-    // console.log(username + website);
+    console.log(username + website); //hitesh   youtube
   }
-  // console.log(website);
+  
+  //console.log(website); //error
 }
-// console.log(username);
 
-//---------------- Intresting ------------------
+//console.log(username); //error
 
-//1 way: Basic function
-console.log(addone(5));
+
+
+//---------------- Intro to Hoisting ------------------
+
+//M1 Basic function
+console.log(addone(5)); //6
 
 function addone(num) {
   return num + 1;
 }
 
-// console.log(addone(5));
+console.log(addone(5)); //6
 
-//2 way: Function expression
-// console.log(addTwo(5));//not possible
+//M2: function expression
 
-const addTwo = function (num) {
+//console.log(addTwo(5)); //error
+
+const addTwo = function (num) { //
   return num + 2;
 };
 
-console.log(addTwo(5));
+console.log(addTwo(5)); //7
+
