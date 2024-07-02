5b87eb2e74d6cc29ffd416aeee6842c017b494b5
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-07-02 20:53:27 +0530

    Update 03_objects.js

--- src/02_Basics/03_objects.js
+++ src/02_Basics/03_objects.js
@@ -4,7 +4,7 @@ const mySym = Symbol("key1");
 const JsUser = {
   name: "Hitesh",
   "full name": "Hitesh Chaudhary",
-  [mySym]: "mykey1",
+  [mySym]: "mykey1", //[Symbol(key1)]: 'mykey1'
   age: 18,
   location: "Jaipur",
   email: "hitesh@google.com",
@@ -12,24 +12,37 @@ const JsUser = {
   lastLoginDay: ["Monday", "Saturday"]
 };
 
-// console.log(JsUser["name"]);
-// console.log(JsUser["full name"]);
-// console.log(JsUser["mySym"]);
-// console.log(typeof JsUser[mySym]);
+console.log(JsUser["name"]);
+console.log(JsUser["full name"]);
+console.log(JsUser["mySym"]); //undefined
+console.log(JsUser[mySym]); //mykey1
+console.log(typeof JsUser[mySym]); //string
 
-// JsUser.email = "hitesh@chatgpt.com"
-// console.log(JsUser);
-// Object.freeze(JsUser);
-// JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+console.log('\n');
+
+JsUser.email = "hitesh@chatgpt.com"
+console.log(JsUser);
+
+console.log('\n');
+
+//Object.freeze(JsUser); //new changes would not occur
+JsUser.email = "hitesh@microsoft.com"
+console.log(JsUser); //same as previous, no error
+
+console.log('\n');
+
+//Function as a key
 
 JsUser.greeting = function () {
   console.log("Hello JS User");
 };
+ 
+console.log(JsUser.greeting); //Op.: [Function (anonymous)] -> Reference of a function
+JsUser.greeting(); //Op.: Hello JS User -> Instance of the function
 
-JsUser.greetingTwo = function () {
-  console.log(`Hello JS User, ${this.name}`);
-};
+console.log('\n');
 
-JsUser.greeting();
-console.log(JsUser.greeting);
+
+// JsUser.greetingTwo = function () {
+//   console.log(`Hello JS User, ${this.name}`);
+// };
