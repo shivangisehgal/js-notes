4a211c271193b1bc7b27a9aaae4895c48268b62c
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2024-07-02 22:51:57 +0530

    Update 03_arrow.js

--- src/03_Basics/03_arrow.js
+++ src/03_Basics/03_arrow.js
@@ -4,16 +4,15 @@ const user = {
   username: "hitesh",
   price: 999,
   welcomeMessage: function () {
-    console.log(`${this.username}, Welcome to website `);
-    // console.log(this);
+    console.log(this); 
   }
 };
 
-// user.welcomeMessage();
-// user["username"] = "sam"
-// user.welcomeMessage();
+user.welcomeMessage(); //this gives the current context, which is this object
+user["username"] = "sam"
+user.welcomeMessage(); //changed context 
 
-// console.log(this);
+console.log(this); //empty object, context of node is empty.
 
 //Not able to access this keyword in normal function
 /*
@@ -23,6 +22,7 @@ function chai(){
 }
 chai();
 */
+
 
 //Not able to access this keyword in function expression
 /*
@@ -34,28 +34,27 @@ chai();
 */
 
 // Arrow function
-/*
+
 const chai = () => {
     let username = "Hitesh"
     console.log(this.username);
 }
-chai();
-*/
+chai(); //undefined, context is not defined in arrow function.
 
-// arrow function with return, or explicitly return
-/*
+// arrow function with return/explicit return
+
 const addTwo = (num1, num2) => {
     return num1 + num2;
 }
-*/
 
-// arrow function with implicit return
-// const addTwo = (num1, num2) => num1 + num2;
+// arrow function implicit
 
-// arrow function with paranthesis
-const addTwo = (num1, num2) => num1 + num2;
-console.log(addTwo(5, 6));
+const addTwoImplicit = (num1, num2) => num1 + num2;
 
-// arrow function with object return using implicit return
+// implicit arrow function with paranthesis
+const addTwoImpBracket = (num1, num2) => (num1 + num2);
+console.log(addTwoImpBracket(5, 6)); //11
+
+// IMPLICIT ARROW FUNCTIONS HAVING OBJECTS NEED TO USE PARENTHISES, else undefined.
 const sampleaddTwo = (num1, num2) => ({ username: "hitesh" });
 console.log(sampleaddTwo(3, 4));
